aefc4fb570b9af43a106c2bbc2400be6fa3c6a89
Mohit Khatri <mohit4bug@gmail.com>
2022-12-08 16:00:10 +0530

    Linear Search Created

---
+++ linearSearch.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+bool linearSearch(int arr[], int size, int value)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == value)
+        {
+            return true;
+            break;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int value;
+    cout << "Enter the value you want to search: ";
+    cin >> value;
+    cout << linearSearch(arr, 10, value);
+
+    // Returns 1 if present and 0 if not present
+
+    return 0;
+}
